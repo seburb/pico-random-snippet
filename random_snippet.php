167c89ad3120027c894c34d9e5fbc6f1748c2700
seburb <sebastian.urban@gmail.com>
2015-03-24 19:27:20 +0100

    updated comment

--- random_snippet.php
+++ random_snippet.php
@@ -1,20 +1,12 @@
 <?php
-header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
-header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+header("Cache-Control: no-cache, must-revalidate"); // Dirty fix to force the browser to update on
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // every page load. Uncomment if cache is needed.
 
-
-/**
- * Example hooks for a Pico plugin
- *
- * @author Gilbert Pellegrom
- * @link http://pico.dev7studios.com
- * @license http://opensource.org/licenses/MIT
- */
 class Random_Snippet {
 
 	public function before_render(&$twig_vars, &$twig)
 	{
-	       $f_snippets = file ("plugins/pico_random_header/snippets.txt");
+	       $f_snippets = file ("plugins/pico-random-snippet/snippets.txt");
 	       $twig_vars['random_snippet'] = $f_snippets[array_rand ($f_snippets)];
 	}
 	
